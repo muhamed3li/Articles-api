c175a620228fa48b92974bb5a8b78205ed4d6f7a
Mohamed Ali <52932544+muhamed3li@users.noreply.github.com>
2022-02-13 19:58:35 +0200

    first commit

---
+++ app/Http/Resources/ArticalResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticalResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        return [
+          'id' => $this->id ,
+          'title' => $this->title ,
+          'body' => $this->body ,
+          'user_id' => $this->user_id ,
+          'user' => new UserResource($this->user) ,
+          'created' => $this->created_at->diffForHumans() ,
+        ];
+    }
+    public function with($request)
+    {
+      return [
+        'version' => '1.0.0',
+        'author' => 'Mohamed Ali'
+      ];
+    }
+}
---
+++ database/factories/ArticalFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title'=>$this->faker->name(),
+            'body'=>$this->faker->sentence(10),
+            'user_id'=>1,
+        ];
+    }
+}
